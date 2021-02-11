1e6aa2c1830d2989cdc9e4b8898b761328694828
Rick Brown <ricky.brown.00@gmail.com>
2021-02-11 09:54:57 +0000

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Rick Brown on 11/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
